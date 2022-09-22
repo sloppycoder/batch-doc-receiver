fb23c7aa07c5662f67bd784109e5b22f2e22709f
Li Lin <guru.lin@gmail.com>
2022-09-22 13:47:18 +0800

    update entity class to avoid @Data https://www.jpa-buddy.com/blog/lombok-and-jpa-what-may-go-wrong/

--- src/main/java/org/vino9/lib/batchdocreceiver/entity/Document.java
+++ src/main/java/org/vino9/lib/batchdocreceiver/entity/Document.java
@@ -6,15 +6,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Data;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
 @Entity
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 public class Document {
 
